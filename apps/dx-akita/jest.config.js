1f1c21c5bf9295a929fa992ae60ee1299770110c
hakimio <sandshrew@gmail.com>
2020-05-29 11:18:48 +0200

    initial commit

---
+++ apps/dx-akita/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'dx-akita',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/dx-akita',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
